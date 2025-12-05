ffaa2bcec7585935368d2f126347e86c32583b20
NEPECON <nepeconunb@gmail.com>
2025-12-05 16:45:26 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="General Accounting Lab", layout="wide")
+
+st.title("General Accounting Lab")
+st.write("Aplicação conectada com GitHub e Streamlit Cloud.")
+st.success("Agora o repositório existe! Já podemos fazer o deploy no Streamlit.")
